5f6b1a1c48d8d53d019cdeec5d4e3ed21d7c5df0
yanaivantsova <yanaivantsova@yandex.by>
2023-08-28 18:31:02 +0200

    feat: add task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    middle;
+    constructor() { }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.middle = Math.round((this.min + this.max) / 2);
     }
 
     lower() {
-
+        return this.max = this.middle;
     }
 
     greater() {
-
+        return this.min = this.middle;
     }
+
 }
 
 module.exports = GuessingGame;
